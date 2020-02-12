ac0cf9f833fe3bab30732f6507b0426d97dcd1f2
mitrashah94 <mitrashah94@gmail.com>
2020-02-12 13:15:59 -0500

    first commit

---
+++ buildmyworld/script/hello.cpp
@@ -0,0 +1,16 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+  class WorldPluginBuildMyWorld : public WorldPlugin
+  {
+  public: WorldPluginBuildMyWorld() : WorldPlugin()
+    {
+      printf("Welcome to Mitra's World!\n");
+    }
+  public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+    {
+    }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginBuildMyWorld)
+}
